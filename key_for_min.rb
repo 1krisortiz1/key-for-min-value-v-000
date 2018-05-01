b5855cb229b7ff34d69e14bbd87d681724e9a155
Kristin Ortiz <itskristin.ortiz@gmail.com>
2018-05-01 16:38:48 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -9,6 +9,9 @@ def key_for_min_value(name_hash)
     name_hash.each do |key, value|
       if min_value == nil
         min_key = key
+        min_value = value 
+      elsif value < min_value
+        min_key = key
         min_value = value
 end
 end
