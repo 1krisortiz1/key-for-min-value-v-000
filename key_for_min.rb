ae85a50bf30d302f0f1a07e2806c4767289ee1bd
Kristin Ortiz <itskristin.ortiz@gmail.com>
2018-05-01 16:31:12 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,10 +5,8 @@ def key_for_min_value(name_hash)
   #tracking variables
   min_key = nil
   min_value = nil
-  #iterator 
+  #iterator
     name_hash.each do |key, value|
-  
-  end
-end
+
 end
 end
